e12648b61f2511fc8340f4b4d5e8d0117a8e8a3f
Mathis Rebel <02rema1bif@hft-stuttgart.de>
2022-05-09 14:34:16 +0200

    added error

--- cicd-drone/src/main/java/hftstuttgart/Calculator.java
+++ cicd-drone/src/main/java/hftstuttgart/Calculator.java
@@ -11,7 +11,7 @@ public class Calculator {
     }    
 
     public int prod(int op1, int op2) {
-        int sum = 1;
+        int sum = 0; // Error in here
         sum *= op1;
         sum *= op2;
         return sum;
